0b2cdd7602af092009eb367434f0ddb1344c00cc
PanameraXXX <xujian727292879@126.com>
2023-08-02 22:34:26 +0800

    fetal: 9_5 finished

---
+++ 9/9_5.py
@@ -0,0 +1,127 @@
+import os
+import torch
+from d2l import torch as d2l
+
+# @save
+d2l.DATA_HUB['fra-eng'] = (d2l.DATA_URL + 'fra-eng.zip',
+                           '94646ad1522d915e7b0f9296181140edcf86a4f5')
+
+
+# @save
+def read_data_nmt():
+    """载入“英语－法语”数据集"""
+    data_dir = d2l.download_extract('fra-eng')
+    with open(os.path.join(data_dir, 'fra.txt'), 'r',
+              encoding='utf-8') as f:
+        return f.read()
+
+
+# @save
+def preprocess_nmt(text):
+    """预处理“英语－法语”数据集"""
+
+    def no_space(char, prev_char):
+        return char in set(',.!?') and prev_char != ' '
+
+    # 使用空格替换不间断空格
+    # 使用小写字母替换大写字母
+    text = text.replace('\u202f', ' ').replace('\xa0', ' ').lower()
+    # 在单词和标点符号之间插入空格
+    out = [' ' + char if i > 0 and no_space(char, text[i - 1]) else char
+           for i, char in enumerate(text)]
+    return ''.join(out)
+
+
+# @save
+def tokenize_nmt(text, num_examples=None):
+    """词元化“英语－法语”数据数据集"""
+    source, target = [], []
+    for i, line in enumerate(text.split('\n')):
+        if num_examples and i > num_examples:
+            break
+        parts = line.split('\t')
+        if len(parts) == 2:
+            source.append(parts[0].split(' '))
+            target.append(parts[1].split(' '))
+    return source, target
+
+
+# @save
+def show_list_len_pair_hist(legend, xlabel, ylabel, xlist, ylist):
+    """绘制列表长度对的直方图"""
+    d2l.set_figsize()
+    _, _, patches = d2l.plt.hist(
+        [[len(l) for l in xlist], [len(l) for l in ylist]])
+    d2l.plt.xlabel(xlabel)
+    d2l.plt.ylabel(ylabel)
+    for patch in patches[1].patches:
+        patch.set_hatch('/')
+    d2l.plt.legend(legend)
+
+
+# @save
+def truncate_pad(line, num_steps, padding_token):
+    """截断或填充文本序列"""
+    if len(line) > num_steps:
+        return line[:num_steps]  # 这里直接截断就行了？？？
+    return line + [padding_token] * (num_steps - len(line))  # 填充
+
+
+# @save
+def build_array_nmt(lines, vocab, num_steps):
+    """将机器翻译的文本序列转换成小批量"""
+    lines = [vocab[l] for l in lines]
+    lines = [l + [vocab['<eos>']] for l in lines]
+    array = torch.tensor([truncate_pad(
+        l, num_steps, vocab['<pad>']) for l in lines])
+    valid_len = (array != vocab['<pad>']).type(torch.int32).sum(1)
+    # 整个代码行的目的是计算array中每一行中不等于vocab['<pad>']的元素个数，
+    return array, valid_len
+
+
+# @save
+def load_data_nmt(batch_size, num_steps, num_examples=600):
+    """返回翻译数据集的迭代器和词表"""
+    text = preprocess_nmt(read_data_nmt())
+    source, target = tokenize_nmt(text, num_examples)
+    src_vocab = d2l.Vocab(source, min_freq=2,
+                          reserved_tokens=['<pad>', '<bos>', '<eos>'])
+    tgt_vocab = d2l.Vocab(target, min_freq=2,
+                          reserved_tokens=['<pad>', '<bos>', '<eos>'])
+    src_array, src_valid_len = build_array_nmt(source, src_vocab, num_steps)
+    tgt_array, tgt_valid_len = build_array_nmt(target, tgt_vocab, num_steps)
+    data_arrays = (src_array, src_valid_len, tgt_array, tgt_valid_len)
+    data_iter = d2l.load_array(data_arrays, batch_size)  # 这个的用法需要了解下，iter的运行机理？
+    return data_iter, src_vocab, tgt_vocab
+
+
+if __name__ == '__main__':
+    raw_text = read_data_nmt()
+    print(raw_text[:75])
+
+    text = preprocess_nmt(raw_text)
+    print(text[:80])
+
+    source, target = tokenize_nmt(text)
+    print(source[:6])
+    print(target[:6])
+
+    # show_list_len_pair_hist(['source', 'target'], '# tokens per sequence',
+    #                         'count', source, target)
+    """
+    使用单词级词元化时，词表大小将明显大于使用字符级词元化时的词表大小。 为了缓解这一问题，这里我们将出现次数少于2次的低频率词元 视为相同的未知（“<unk>”）词元。 除此之外，我们还指定了额外的特定词元， 例如在小批量时用于将序列填充到相同长度的填充词元（“<pad>”）， 以及序列的开始词元（“<bos>”）和结束词元（“<eos>”）。 这些特殊词元在自然语言处理任务中比较常用。
+    """
+    src_vocab = d2l.Vocab(source, min_freq=2,
+                          reserved_tokens=['<pad>', '<bos>', '<eos>'])
+
+    print(truncate_pad(src_vocab[source[0]], 10, src_vocab['<pad>']))
+
+    train_iter, src_vocab, tgt_vocab = load_data_nmt(batch_size=2, num_steps=8)
+    for X, X_valid_len, Y, Y_valid_len in train_iter:
+        print('X:', X.type(torch.int32))
+        print('X的有效长度:', X_valid_len)
+        print('Y:', Y.type(torch.int32))
+        print('Y的有效长度:', Y_valid_len)
+        break
+
+    pass
